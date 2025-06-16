00020b5b4506ee61b75806076c33335421fa32ea
Ahmad Fauzan <gavier.exe@gmail.com>
2025-06-16 20:44:28 +0700

    lib: SDL

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,8 +1,16 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
   std::cout << "Welcome to Throne Engine!\n";
+
+  if (SDL_Init(SDL_INIT_VIDEO))
+  {
+    std::cout << "SDL initialized successfully!\n";
+  }
 
   return 0;
 }
