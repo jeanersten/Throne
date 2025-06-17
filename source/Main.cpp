72de639784a6c9dce2100aed3b2456c27fffecd0
Ahmad Fauzan <gavier.exe@gmail.com>
2025-06-17 21:53:05 +0700

    lib: spdlog

--- source/Main.cpp
+++ source/Main.cpp
@@ -2,11 +2,15 @@
 
 #include <SDL3/SDL_main.h>
 
+#include <spdlog/spdlog.h>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
   std::cout << "Welcome to Throne Engine!\n";
+
+  spdlog::error("TEST");
 
   Game main_game{ }; 
   main_game.run();
